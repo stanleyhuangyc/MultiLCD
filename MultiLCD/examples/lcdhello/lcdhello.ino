94ee09308e5e5986adbb992c6fd48a7a05bc2008
Stanley Huang <stanleyhuangyc@gmail.com>
2013-05-29 00:19:23 +0800

    update example sketch

--- MultiLCD/examples/lcdhello/lcdhello.ino
+++ MultiLCD/examples/lcdhello/lcdhello.ino
@@ -1,20 +1,47 @@
+/*************************************************************************
+* Demo sketch of Arduino Text Display Library for Multiple LCDs
+* Distributed under GPL v2.0
+* Copyright (c) 2013 Stanley Huang <stanleyhuangyc@live.com>
+* All rights reserved.
+*************************************************************************/
+
 #include <Arduino.h>
 #include <Wire.h>
 #include <MultiLCD.h>
 
-//LCD_OLED lcd; /* for I2C OLED module */
+LCD_SSD1306 lcd; /* for SSD1306 OLED module */
 //LCD_PCD8544 lcd; /* for LCD4884 shield or Nokia 5100 screen module */
-LCD_1602 lcd; /* for LCD1602 shield */
+//LCD_1602 lcd; /* for LCD1602 shield */
+//LCD_ZTOLED lcd; /* for ZT OLED module */
 
 void setup()
 {
 	lcd.begin();
+	lcd.clear();
+
 	lcd.setCursor(0, 0);
-	lcd.print("Hello, World");
+	lcd.print("Hello, world!");
+
 	lcd.setCursor(0, 1);
-	lcd.printLarge("12345");
+	lcd.printLong(1234567890, FONT_SIZE_SMALL);
+
+	lcd.setCursor(0, 2);
+	lcd.printLong(1234567890, FONT_SIZE_MEDIUM);
+
+	lcd.setCursor(0, 3);
+	lcd.printLong(12345678, FONT_SIZE_LARGE);
+
+	//pinMode(8, INPUT);
 }
 
 void loop()
 {
+    /*
+  	lcd.setCursor(0, 7);
+    if (digitalRead(8)) {
+        lcd.write('+');
+    } else {
+        lcd.write('-');
+    }
+    */
 }
